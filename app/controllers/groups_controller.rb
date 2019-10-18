25a69bf77b9cf78f9a33d4ca4dc8ee7c7d701253
seme0702 <56156442+seme0702@users.noreply.github.com>
2019-10-18 13:32:38 +0900

    非同期

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-
+  before_action :set_group, only: [:edit, :update]
   def index
   end
 
@@ -17,14 +17,6 @@ class GroupsController < ApplicationController
     end
   end
 
-  private
-  def group_params
-    params.require(:group).permit(:name, { :user_ids => [] })
-  end
-
-  def edit
-  end
-
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -33,7 +25,18 @@ class GroupsController < ApplicationController
     end
   end
 
+
+  
+  private
+  def group_params
+    params.require(:group).permit(:name, { :user_ids => [] })
+  end
+
+  def edit
+  end
+
   def set_group
     @group = Group.find(params[:id])
   end
 end
+
